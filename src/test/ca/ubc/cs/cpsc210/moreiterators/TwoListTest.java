2d109ff9716be7012c88dd44b63311147071e985
Paul Carter <pcarter@cs.ubc.ca>
2020-12-01 14:30:24 -0800

    Updated tests - uses List<Integer> instead of array

--- src/test/ca/ubc/cs/cpsc210/moreiterators/TwoListTest.java
+++ src/test/ca/ubc/cs/cpsc210/moreiterators/TwoListTest.java
@@ -1,110 +1,183 @@
 package ca.ubc.cs.cpsc210.moreiterators;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class TwoListTest {
-    private TwoLists twoLists = new TwoLists();
+    private TwoLists twoLists;
+
+    @BeforeEach
+    void runBefore() {
+        twoLists = new TwoLists();
+    }
 
     @Test
-    public void oneThenTwoTest() {
-        int expected[] = { 1, 2, 3, 4, 5, 11, 12, 13 };
-        twoLists.add(1, 1); twoLists.add(1, 2); twoLists.add(1, 3); twoLists.add(1, 4); twoLists.add(1, 5);
-        twoLists.add(2, 11); twoLists.add(2, 12); twoLists.add(2, 13);
+    void oneThenTwoTest() {
+        List<Integer> expected = Arrays.asList(1, 2, 3, 4, 5, 11, 12, 13);
+        twoLists.add(1, 1);
+        twoLists.add(1, 2);
+        twoLists.add(1, 3);
+        twoLists.add(1, 4);
+        twoLists.add(1, 5);
+        twoLists.add(2, 11);
+        twoLists.add(2, 12);
+        twoLists.add(2, 13);
 
-        twoLists.chooseIterator(TwoLists.OneThenTwo);
+        twoLists.chooseIterator(TwoLists.ONE_THEN_TWO);
         Iterator<Integer> it = twoLists.iterator();
         verify(expected, it);
     }
 
     @Test
-    public void alternatingTest() {
-        int expected[] = { 1, 11, 2, 12, 3, 13, 4, 5 };
-        twoLists.add(1, 1); twoLists.add(1, 2); twoLists.add(1, 3); twoLists.add(1, 4); twoLists.add(1, 5);
-        twoLists.add(2, 11); twoLists.add(2, 12); twoLists.add(2, 13);
+    void alternatingTest() {
+        List<Integer> expected = Arrays.asList(1, 11, 2, 12, 3, 13, 4, 5);
+        twoLists.add(1, 1);
+        twoLists.add(1, 2);
+        twoLists.add(1, 3);
+        twoLists.add(1, 4);
+        twoLists.add(1, 5);
+        twoLists.add(2, 11);
+        twoLists.add(2, 12);
+        twoLists.add(2, 13);
 
-        twoLists.chooseIterator(TwoLists.Alternating);
+        twoLists.chooseIterator(TwoLists.ALTERNATING);
         Iterator<Integer> it = twoLists.iterator();
         verify(expected, it);
     }
 
     @Test
-    public void cartesianTest() {
-        int expected[] = { 11, 12, 13, 22, 24, 26, 33, 36, 39, 44, 48, 52, 55, 60, 65 };
-        twoLists.add(1, 1); twoLists.add(1, 2); twoLists.add(1, 3); twoLists.add(1, 4); twoLists.add(1, 5);
-        twoLists.add(2, 11); twoLists.add(2, 12); twoLists.add(2, 13);
+    void cartesianTest() {
+        List<Integer> expected = Arrays.asList(11, 12, 13, 22, 24, 26, 33, 36, 39, 44, 48, 52, 55, 60, 65);
+        twoLists.add(1, 1);
+        twoLists.add(1, 2);
+        twoLists.add(1, 3);
+        twoLists.add(1, 4);
+        twoLists.add(1, 5);
+        twoLists.add(2, 11);
+        twoLists.add(2, 12);
+        twoLists.add(2, 13);
 
-        twoLists.chooseIterator(TwoLists.Cartesian);
+        twoLists.chooseIterator(TwoLists.CARTESIAN);
         Iterator<Integer> it = twoLists.iterator();
         verify(expected, it);
     }
 
     @Test
-    public void oneThenTwoOneEmptyTest() {
-        int expected[] = { 11, 12, 13 };
-        twoLists.add(2, 11); twoLists.add(2, 12); twoLists.add(2, 13);
+    void oneThenTwoFirstEmptyTest() {
+        List<Integer> expected = Arrays.asList(11, 12, 13);
+        twoLists.add(2, 11);
+        twoLists.add(2, 12);
+        twoLists.add(2, 13);
 
-        twoLists.chooseIterator(TwoLists.OneThenTwo);
+        twoLists.chooseIterator(TwoLists.ONE_THEN_TWO);
         Iterator<Integer> it = twoLists.iterator();
         verify(expected, it);
     }
 
     @Test
-    public void alternatingOneEmptyTest() {
-        int expected[] = { 11, 12, 13 };
-        twoLists.add(2, 11); twoLists.add(2, 12); twoLists.add(2, 13);
+    void alternatingFirstEmptyTest() {
+        List<Integer> expected = Arrays.asList(11, 12, 13);
+        twoLists.add(2, 11);
+        twoLists.add(2, 12);
+        twoLists.add(2, 13);
 
-        twoLists.chooseIterator(TwoLists.Alternating);
+        twoLists.chooseIterator(TwoLists.ALTERNATING);
         Iterator<Integer> it = twoLists.iterator();
         verify(expected, it);
     }
 
     @Test
-    public void cartesianOneEmptyTest() {
-        int expected[] = { };
-        twoLists.add(2, 11); twoLists.add(2, 12); twoLists.add(2, 13);
+    void cartesianFirstEmptyTest() {
+        List<Integer> expected = new ArrayList<Integer>();
+        twoLists.add(2, 11);
+        twoLists.add(2, 12);
+        twoLists.add(2, 13);
 
-        twoLists.chooseIterator(TwoLists.Cartesian);
+        twoLists.chooseIterator(TwoLists.CARTESIAN);
         Iterator<Integer> it = twoLists.iterator();
         verify(expected, it);
     }
 
     @Test
-    public void oneThenTwoOTwoEmptyTest() {
-        int expected[] = { 1, 2, 3, 4, 5 };
-        twoLists.add(1, 1); twoLists.add(1, 2); twoLists.add(1, 3); twoLists.add(1, 4); twoLists.add(1, 5);
+    void oneThenTwoSecondEmptyTest() {
+        List<Integer> expected = Arrays.asList(1, 2, 3, 4, 5);
+        twoLists.add(1, 1);
+        twoLists.add(1, 2);
+        twoLists.add(1, 3);
+        twoLists.add(1, 4);
+        twoLists.add(1, 5);
 
-        twoLists.chooseIterator(TwoLists.OneThenTwo);
+        twoLists.chooseIterator(TwoLists.ONE_THEN_TWO);
         Iterator<Integer> it = twoLists.iterator();
         verify(expected, it);
     }
 
     @Test
-    public void alternatingTwoEmptyTest() {
-        int expected[] = { 1, 2, 3, 4, 5 };
-        twoLists.add(1, 1); twoLists.add(1, 2); twoLists.add(1, 3); twoLists.add(1, 4); twoLists.add(1, 5);
+    void alternatingSecondEmptyTest() {
+        List<Integer> expected = Arrays.asList(1, 2, 3, 4, 5);
+        twoLists.add(1, 1);
+        twoLists.add(1, 2);
+        twoLists.add(1, 3);
+        twoLists.add(1, 4);
+        twoLists.add(1, 5);
 
-        twoLists.chooseIterator(TwoLists.Alternating);
+        twoLists.chooseIterator(TwoLists.ALTERNATING);
         Iterator<Integer> it = twoLists.iterator();
         verify(expected, it);
     }
 
     @Test
-    public void cartesianTwoEmptyTest() {
-        int expected[] = { };
-        twoLists.add(1, 1); twoLists.add(1, 2); twoLists.add(1, 3); twoLists.add(1, 4); twoLists.add(1, 5);
+    void cartesianTwoSecondEmptyTest() {
+        List<Integer> expected = new ArrayList<Integer>();
+        twoLists.add(1, 1);
+        twoLists.add(1, 2);
+        twoLists.add(1, 3);
+        twoLists.add(1, 4);
+        twoLists.add(1, 5);
 
-        twoLists.chooseIterator(TwoLists.Cartesian);
+        twoLists.chooseIterator(TwoLists.CARTESIAN);
         Iterator<Integer> it = twoLists.iterator();
         verify(expected, it);
     }
 
-    private void verify(int[] expected, Iterator<Integer> it) {
-        for (int i = 0; i < expected.length; i++) {
-            assertEquals(expected[i], it.next());
+    @Test
+    void oneThenTwoBothEmptyTest() {
+        List<Integer> expected = new ArrayList<Integer>();
+
+        twoLists.chooseIterator(TwoLists.ONE_THEN_TWO);
+        Iterator<Integer> it = twoLists.iterator();
+        verify(expected, it);
+    }
+
+    @Test
+    void alternatingBothEmptyTest() {
+        List<Integer> expected = new ArrayList<Integer>();
+
+        twoLists.chooseIterator(TwoLists.ALTERNATING);
+        Iterator<Integer> it = twoLists.iterator();
+        verify(expected, it);
+    }
+
+    @Test
+    void cartesianTwoBothEmptyTest() {
+        List<Integer> expected = new ArrayList<Integer>();
+
+        twoLists.chooseIterator(TwoLists.CARTESIAN);
+        Iterator<Integer> it = twoLists.iterator();
+        verify(expected, it);
+    }
+
+    private void verify(List<Integer> expected, Iterator<Integer> it) {
+        for (Integer next : expected) {
+            assertEquals(next, it.next());
         }
         assertFalse(it.hasNext());
     }
