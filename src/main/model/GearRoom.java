09553c02646be5bc5ee38d43041c77c16978e109
Shravan Kumar <kshravan694@gmail.com>
2024-10-08 19:54:51 -0700

    Implemented GearRoom for user story 6

--- src/main/model/GearRoom.java
+++ src/main/model/GearRoom.java
@@ -1,19 +1,23 @@
 package model;
 import java.util.List;
+import java.util.ArrayList;
 
 public class GearRoom {
+    private List<Gear> gearRoom;
 
     GearRoom() {
-
+        this.gearRoom = new ArrayList<>();
     }
 
     //MODIFIES: this
-    //EFFECTS: Adds gear to the gearroom and makes it available for trips to use
+    //EFFECTS: Adds gear to the gearroom and makes it available for trips to use,
+    //         latest added gear is last
     public void addGear(Gear g){
-
+        gearRoom.add(g);
     }
 
-    public void getGearRoom() {
+    public List<Gear> getGearRoom() {
 
+        return gearRoom;
     }
 }
