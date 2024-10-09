e92a739a461dc354a6aa6a34c2a4e1367c50fab2
Shravan Kumar <kshravan694@gmail.com>
2024-10-08 19:53:19 -0700

    Added tests to GearRoom for user story 6

---
+++ src/test/model/TestGearRoom.java
@@ -0,0 +1,48 @@
+package model;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestGearRoom {
+    GearRoom testGearRoom;
+    Gear g1;
+    Gear g2;
+    List<Gear> gr;
+
+    @BeforeEach
+    void runBefore(){
+        testGearRoom = new GearRoom();
+        g1 = new Gear();
+        g2 = new Gear();
+        gr = testGearRoom.getGearRoom();
+    }
+
+    @Test
+    void testConstructor(){
+        assertTrue(gr.isEmpty());
+    }
+
+    @Test
+    void testAddGear(){
+        testGearRoom.addGear(g1);
+        assertEquals(1, gr.size());
+        assertEquals(g1, gr.get((0)));
+    }
+    @Test
+    void testAddMultipleGear() {
+        testGearRoom.addGear(g1);
+        testGearRoom.addGear(g2);
+        assertEquals(2, gr.size());
+        assertEquals(g1, gr.get(0));
+        assertEquals(g2, gr.get(1));
+    }
+
+
+    
+
+
+}
