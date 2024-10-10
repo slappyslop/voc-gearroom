de2e2c64f1b26d293a6b90b6c95427d374cb5462
Shravan Kumar <kshravan694@gmail.com>
2024-10-09 22:06:27 -0700

    Implemented Gear

--- src/main/model/Gear.java
+++ src/main/model/Gear.java
@@ -1,24 +1,43 @@
 package model;
 
-public class Gear {
+import java.util.ArrayList;
+import java.util.List;
 
-    public Gear() {}
+public class Gear {
+    private String name;
+    private List<Integer> reservations;
+
+    public Gear(String name) {
+        this.name = name;
+        this.reservations = new ArrayList<Integer>();
+    }
 
     public String getName() {
-        return "A";
+        return name;
     }
-    //REQUIRES: 0 <= start <= end
+    //REQUIRES: 0 <= start <= end, Shouldn't already be reserved on those dates
     //MODIFIES: this    
-    //EFFECTS: Marks the gear as reserved over a specific date range
+    //EFFECTS: Marks the gear as reserved over a specific date range (inclusive)
     public void reserve(int start, int end) {
-        //TODO 
+        for(int i = start; i <= end; i++) {
+            reservations.add(i);
+        }
+    }
+
+    public List<Integer> getReservations() {
+        return reservations;
     }
 
 
     //REQUIRES: 0 <= start <= end
     //EFFECTS: Returns true if a piece of gear is reserved over a range of dates
     public boolean isReserved(int start, int end) {
+        for (int i : reservations) {
+            if ((start <= i) && (i <= end)) {
+                return true;
+            } 
+        }
         return false;
-    }
+    } 
 
 }
