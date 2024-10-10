92d84ce93b11e9db6c1b106bc8f3fb973e9707ac
Shravan Kumar <kshravan694@gmail.com>
2024-10-09 19:22:16 -0700

    Finished implementing trip for user stories 3, 4, 5, 7

--- src/main/model/Trip.java
+++ src/main/model/Trip.java
@@ -1,63 +1,55 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Trip {
 
+    private List<Member> going;
+    private List<Member> committed;
+    private List<Member> interested;
+
     public Trip(){
+        going = new ArrayList<Member>();
+        committed = new ArrayList<Member>();
+        interested = new ArrayList<Member>();
 
 
     }
-
-    //MODIFIES: this, m
+    //REQUIRES: Must be invoked by trip leader only
+    //MODIFIES: this
     //EFFECTS: Adds the member to the going list of the trip
     public void addToGoing(Member m) {
-        //TODO
+        going.add(m);
 
     }
     //MODIFIES: this
     //EFFECTS: Adds the member to the committed list of the trip
     public void addToCommitted(Member m){
-        //TODO
+        committed.add(m);
     }
 
     //MODIFIES: this
     //EFFECTS: Adds the member to the committed list of the trip
     public void addToInterested(Member m){
+        interested.add(m);
 
-    }
-
-    //EFFECTS: Gets size of the trip
-    public int getSize() {
-        //TODO: stub
-        return 0;
-    }
-
-    //EFFECTS: Returns max gear required for trip 
-    public List<Gear> getMaxGear(){
-        return null;
     }
 
     //EFFECTS: Returns list of going members on trip
     public List<Member> getGoing() {
-        return null;
+        return going;
     }
 
     //EFFECTS: Returns list of committed members
     public List<Member> getCommitted() {
-        return null;
+        return committed;
     }
 
     //EFFECTS: Returns list of interested members
     public List<Member> getInterested() {
-        return null;
+        return interested;
 
     }
-
-    //EFFECTS: Returns date of trip 
-    public int getDate(){
-        return 0;
-    }
-
 
 }
