e112d3a983ee53eea9acc05dd50772029c2e8e68
Shravan Kumar <kshravan694@gmail.com>
2024-10-09 22:11:40 -0700

    Implemented Trip

--- src/main/model/Trip.java
+++ src/main/model/Trip.java
@@ -4,15 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Trip {
-
+    
+    private GearRoom gr;
     private List<Member> going;
     private List<Member> committed;
     private List<Member> interested;
+    private int startDate;
+    private int endDate;
 
-    public Trip(){
+    public Trip(GearRoom gr){ 
         going = new ArrayList<Member>();
         committed = new ArrayList<Member>();
         interested = new ArrayList<Member>();
+        this.gr = gr;
 
 
     }
@@ -24,21 +28,35 @@ public class Trip {
 
     }
     //MODIFIES: this
-    //EFFECTS: Adds the member to the committed list of the trip
-    public void addToCommitted(Member m){
+    //EFFECTS: Adds the member to the committed list of the trip, 
+    //         returns true if there is enough gear in the club room
+    //         false if there is not.
+    public boolean addToCommitted(Member m) {
         committed.add(m);
+        return checkEnoughGear();
     }
 
     //MODIFIES: this
     //EFFECTS: Adds the member to the committed list of the trip
     public void addToInterested(Member m){
         interested.add(m);
-
     }
 
     //EFFECTS: Returns list of going members on trip
     public List<Member> getGoing() {
         return going;
+    }
+    //MODIFIES: this, gr
+    //EFFECTS: Checks if there is enough unreserved gear for one additional person in the gear room
+    //         Marks gear as reserved and returns true if there is, returns false if no gear
+    public boolean checkEnoughGear() {
+        for(Gear g : gr.getGearRoom()) {
+            if (!g.isReserved(startDate, endDate)) {
+                g.reserve(startDate, endDate);
+                return true;
+            }
+        }
+        return false;
     }
 
     //EFFECTS: Returns list of committed members
@@ -50,6 +68,22 @@ public class Trip {
     public List<Member> getInterested() {
         return interested;
 
+    }
+
+    public void setStartDate(int start) {
+        this.startDate = start;
+    }
+    
+    public void setEndDate(int end) {
+        this.endDate = end;
+    }
+
+    public int getStartDate() {
+        return startDate;
+    }
+
+    public int getEndDate() {
+        return endDate;
     }
 
 }
