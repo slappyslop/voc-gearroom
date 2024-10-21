50a9e2eb331075b2beb477ec99220737105ca5f6
Shravan Kumar <kshravan694@gmail.com>
2024-10-21 14:46:23 -0700

    Renamed jsongeartests, to jsontests

---
+++ src/test/persistence/JsonTests.java
@@ -0,0 +1,29 @@
+package persistence;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import model.Gear;
+
+import model.Trip;
+
+
+/*Inspired by JSONSERIALIZATIONDEMO 
+ * https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
+*/
+public class JsonTests {
+    protected void checkGear(String name, List<Integer> reservations, Gear gear) {  
+        assertEquals(name, gear.getName());
+        assertEquals(reservations, gear.getReservations());
+    }
+
+    protected void checkTrip(Trip t, Trip jsont) {
+        assertEquals(t.getGoing(), jsont.getGoing());
+        assertEquals(t.getCommitted(), jsont.getCommitted());
+        assertEquals(t.getInterested(), jsont.getInterested());
+        assertEquals(t.getStartDate(), t.getEndDate());
+        assertEquals(t.getGearList(), jsont.getGearList());
+
+    }
+
+}
